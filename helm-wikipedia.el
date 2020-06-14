543425c66d8bf27a73d1ad29d0a1aca447be35e3
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2020-06-14 09:37:05 +0200

    Add commentary section

--- helm-wikipedia.el
+++ helm-wikipedia.el
@@ -20,6 +20,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
+;;; Commentary:
+;; Helm interface to wikipedia suggestions.
+
 ;;; Code:
 
 (require 'json)
